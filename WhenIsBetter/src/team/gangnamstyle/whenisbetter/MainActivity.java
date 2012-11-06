cdc804744b20d8dc3d13387ffca7a59b1e01511b
Sanghun Oh <sanghun123@gmail.com>
2012-11-06 17:39:00 -0500

    sanghun test

--- WhenIsBetter/src/team/gangnamstyle/whenisbetter/MainActivity.java
+++ WhenIsBetter/src/team/gangnamstyle/whenisbetter/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// test
+		// tstfewft
 	}
 
 	@Override
