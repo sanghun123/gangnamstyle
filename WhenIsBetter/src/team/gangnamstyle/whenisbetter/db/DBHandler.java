071f23ca40f0143c3cba8646abe2e9ceddd0bfa5
Jinhyun Kim <jinkim608@gmail.com>
2012-11-19 10:13:43 -0500

    Attempt to connect to db
    - DBHandler includes connection and methods for inserting in table

---
+++ WhenIsBetter/src/team/gangnamstyle/whenisbetter/db/DBHandler.java
@@ -0,0 +1,86 @@
+package team.gangnamstyle.whenisbetter.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class DBHandler {
+
+	JSONParser jsonParser = new JSONParser();
+
+	// url to create new product
+	private static String url_create_product = "http://10.0.2.2/whenisbetter/create_product.php";
+
+	// JSON Node names
+	private static final String TAG_SUCCESS = "success";
+
+	public DBHandler() {
+		new CreateNewProduct().execute();
+	}
+
+	/**
+	 * Background Async Task to Create new product
+	 * */
+	class CreateNewProduct extends AsyncTask<String, String, String> {
+
+		/**
+		 * Before starting background thread Show Progress Dialog
+		 * */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		/**
+		 * Creating product
+		 * */
+		protected String doInBackground(String... args) {
+
+			// Building Parameters
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("name", "test name"));
+			params.add(new BasicNameValuePair("price", "test price"));
+			params.add(new BasicNameValuePair("description", "test description"));
+
+			// getting JSON Object
+			// Note that create product url accepts POST method
+			JSONObject json = jsonParser.makeHttpRequest(url_create_product,
+					"POST", params);
+
+			// check log cat fro response
+			Log.d("Create Response", json.toString());
+
+			// check for success tag
+			try {
+				int success = json.getInt(TAG_SUCCESS);
+
+				if (success == 1) {
+
+					// closing this screen
+					// finish();
+				} else {
+					// failed to create product
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		protected void onPostExecute(String file_url) {
+			// dismiss the dialog once done
+		}
+
+	}
+}
